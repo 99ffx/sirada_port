c60c2ffef178985e541fffceeecc3133bf8f0701
Kittipaisarnkul <sirada.kittipaisarnkul@allianz.com>
2023-07-10 11:02:14 +0700

    change web icon

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,11 @@
+import "./navbar.css"
+import React from 'react'
+
+function navbar() {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default navbar
+
---
+++ src/screen/home.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import navbar from '../components/navbar/navbar'
+
+export default function home() {
+  return (
+    <div>home</div>
+  )
+}
