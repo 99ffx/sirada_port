fd3fdb144c05eb7729eac678d50b369c9f92f45a
99ffx <sirada.2635@gmail.com>
2023-07-13 23:39:15 +0700

    add bootstrap

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,18 +3,23 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className="navbar-wrap">
-      <div className="headline-left">
-        <span>s</span>
-        <span style={{ color: '#96F18E', fontStyle: 'italic' }}>i</span>
-        <span>rada</span>
+    <div className="container">
+      <div className="row">
+        <div className="col-10">
+          <div className="headline-left">
+            <span>s</span>
+            <span style={{ color: '#96F18E', fontStyle: 'italic' }}>i</span>
+            <span>rada</span>
+          </div>
+        </div>
+        <div className="col-2">
+          <div className="contact-me">
+            <button type="button" class="btn btn-outline-primary">Contact Me</button>
+          </div>
+        </div>
       </div>
 
-      <div className="contact-me">
-        <button>
-          Contact Me
-        </button>
-      </div>
+
     </div>
 
 
