3f39804ab1ff02682244938988f23c6d1b54d5e7
99ffx <sirada.2635@gmail.com>
2023-08-21 22:54:27 +0700

    CV new directory

--- src/screen/home.js
+++ src/screen/home.js
@@ -44,7 +44,7 @@ function Home() {
       <div className="headline-sub container">
         <span className="portfolio">PORTFOLIO</span>
         <span className="ux">UX/UI and Web Development</span>
-        <a href="../assests/Sirada-CV-Euro.pdf" download>
+        <a href="./Sirada-CV-Euro.pdf" download>
           <button type="button" className="btn">
             <div
               className="btn-context"
